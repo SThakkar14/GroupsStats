33f57acf53b576b5e1308787988868584b5bedc7
Shubham Thakkar <shubhamt@utexas.edu>
2015-01-07 20:37:20 -0600

    Revert "Updated min sdk version to 4.4"
    This reverts commit 3db186584c5d6556ed95d232c3accea03cf66ca1.

--- app/src/androidTest/java/com/me/shubham/groupsstats/ApplicationTest.java
+++ app/src/androidTest/java/com/me/shubham/groupsstats/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.shubham.groupsthing;
+package com.me.shubham.groupsstats;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
---
+++ app/src/main/java/com/me/shubham/groupsstats/LoginPage.java
@@ -0,0 +1,78 @@
+package com.me.shubham.groupsstats;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBarActivity;
+
+import com.facebook.AppEventsLogger;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+
+import java.util.Arrays;
+
+public class LoginPage extends ActionBarActivity {
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState sessionState, Exception e) {
+            if (session != null && sessionState.isOpened()) {
+                Intent intent = new Intent(LoginPage.this, GroupsDisplayPage.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
+    private UiLifecycleHelper uiLifecycleHelper;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        uiLifecycleHelper = new UiLifecycleHelper(this, callback);
+        uiLifecycleHelper.onCreate(savedInstanceState);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("user_groups", "user_about_me"));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        uiLifecycleHelper.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        uiLifecycleHelper.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiLifecycleHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiLifecycleHelper.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiLifecycleHelper.onSaveInstanceState(outState);
+    }
+
+}
